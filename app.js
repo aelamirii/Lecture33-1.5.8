6e437807896b360e271b941b84acd94e35d7e035
El Amiri <AElAmiri@lear.com>
2021-01-08 16:48:11 +0100

    Lecture33

---
+++ app.js
@@ -0,0 +1,257 @@
+(function () {
+'use strict';
+
+angular.module('ShoppingListComponentApp', [])
+.controller('ShoppingListController', ShoppingListController)
+.factory('ShoppingListFactory', ShoppingListFactory)
+// .directive('shoppingList', ShoppingList)
+.component('shoppingList', {
+    templateUrl: 'shoppingList.html',
+    controller: ShoppingListComponentController,
+    bindings: {
+      listItems: '<',
+      title: '@',
+      onRemove: '&'
+    }
+
+});
+
+
+ShoppingListComponentController.$inject = ['$scope', '$element'];
+function ShoppingListComponentController($scope, $element) {
+
+  var $ctrl_temps = this;
+
+  $ctrl_temps.findcookies = function () {
+
+    for (var i = 0; i < $ctrl_temps.listItems.getItems.length; i++) {
+      var name = $ctrl_temps.listItems.getItems[i].name;
+      console.log("Name : ", name);
+      if(name.toLowerCase().indexOf("cookies") !== -1)
+      { console.log("true");
+      return true;
+      }
+    }
+      return false;
+  };
+
+  $ctrl_temps.remove = function (myIndex) {
+      $ctrl_temps.onRemove( { indexKey : myIndex });
+  };
+
+
+  $ctrl_temps.$onInit = function () {
+    console.log("on Init ");
+  };
+
+  $ctrl_temps.$onChanges = function (changesObj) {
+    console.log("Changes are :", changesObj);
+  };
+
+  $ctrl_temps.$postLink = function () {
+
+    $scope.$watch('$ctrl.findcookies()', function (newValue, oldValue) {
+
+      console.log("$ctrl_temps Find cookies :", $ctrl_temps.findcookies());
+
+      console.log("Old Value :", oldValue);
+      console.log("New Value :", newValue);
+
+      if(newValue === true)
+      {
+        // Show Warning cookies
+        var WarningMessage = $element.find('div.error');
+        WarningMessage.slideDown(800);
+      }
+      else {
+        // Hide Warning cookies
+        var WarningMessage = $element.find('div.error');
+        WarningMessage.slideUp(800);
+      }
+
+
+    });
+
+  };
+
+
+};
+
+
+
+
+// function ShoppingList() {
+//
+//   var ddo = {
+//     templateUrl: 'shoppingList.html',
+//     scope: {
+//       listDirective: '<',
+//       titleDirective: '@',
+//       onRemove: '&'
+//     },
+//     controller: ShoppingListDirectiveController,
+//     controllerAs: 'ListDirectiveController',
+//     bindToController: true,
+//     link: ShoppingListDirectiveLink,
+//     transclude: true
+//
+//   };
+//   return ddo;
+// };
+
+
+// function ShoppingListDirectiveLink(scope, element, attrs, controller) {
+//
+//   console.log("scope :", scope);
+//   console.log("element :", element);
+//   console.log("attrs :", attrs);
+//   console.log("controller :", controller);
+//
+//   scope.$watch('ListDirectiveController.findcookies()', function (newValue, oldValue) {
+//
+//     console.log("Old Value :", oldValue);
+//     console.log("New Value :", newValue);
+//
+//     if(newValue == true)
+//     {
+//       DisplayWarningcookies();
+//     }
+//     else {
+//       RemoveWarningcookies();
+//     }
+//
+//
+//   });
+//
+//   function DisplayWarningcookies() {
+//     //  Using JqLite
+//     // var WarningMessage = element.find("div");
+//     // WarningMessage.css("display", "block");
+//
+//     // Using jQuery
+//     var WarningMessage = element.find("div.error");
+//     WarningMessage.slideDown(800);
+//
+//   }
+//
+//   function RemoveWarningcookies() {
+//     //  Using JqLite
+//     // var WarningMessage = element.find("div");
+//     // WarningMessage.css("display", "none");
+//
+//     // Using jQuery
+//     var WarningMessage = element.find("div.error");
+//     WarningMessage.slideUp(800);
+//
+//   }
+//
+// };
+
+
+// function ShoppingListDirectiveController() {
+//
+//   var list_temps = this;
+//
+//   list_temps.findcookies = function () {
+//
+//     for (var i = 0; i < list_temps.listDirective.getItems.length; i++) {
+//       var name = list_temps.listDirective.getItems[i].name;
+//       if(name.toLowerCase().indexOf("cookies") !== -1)
+//       return true;
+//     }
+//       return false;
+//   };
+//
+// };
+
+
+
+ShoppingListController.$inject = ['ShoppingListFactory'];
+function ShoppingListController(ShoppingListFactory) {
+
+  var list = this;
+
+  var ShoppingList = ShoppingListFactory();
+
+  list.ItemName = "cookies";
+  list.ItemQuantity = "";
+
+  list.getItems = ShoppingList.getItems();
+
+  var Org_Title = "Shopping List 1 ";
+  list.titleController = Org_Title + "("+ list.getItems.length +")";
+
+  list.WarningMessage = "Warning cookies is detected *************** ";
+
+  list.addItem = function () {
+    try {
+      ShoppingList.addItem(list.ItemName, list.ItemQuantity);
+      list.titleController = Org_Title + "("+ list.getItems.length +")";
+    } catch (e) {
+      list.errorMessage = e.message
+    } finally {
+
+    }
+  };
+
+  list.RemoveItem = function (indexItem) {
+      console.log("This is :", this);
+      this.LastRemoved = "Last item removed was "+ list.getItems[indexItem].name;
+      ShoppingList.RemoveItem(indexItem);
+      list.errorMessage = "";
+      this.titleController = Org_Title + "("+ list.getItems.length +")";
+  };
+
+};
+
+
+
+
+
+function ShoppingList_Service(maxItems) {
+
+  var service = this;
+
+  var Items = [];
+
+
+  service.addItem = function (itemName, itemQuantity) {
+
+    if( ( maxItems === undefined ) ||
+        ( maxItems !== undefined && Items.length < maxItems )
+    )
+    {
+      var item = {
+        name: itemName,
+        quantity: itemQuantity
+      };
+
+      Items.push(item);
+    }
+    else {
+      throw new Error("Max items ("+ Items.length +") was reached ");
+    }
+
+  };
+
+  service.getItems = function () {
+    return Items;
+  };
+
+  service.RemoveItem = function (indexItem) {
+    Items.splice( indexItem, 1 );
+  };
+
+};
+
+
+function ShoppingListFactory() {
+
+  var factory = function (maxItems) {
+    return new ShoppingList_Service(maxItems);
+  };
+  return factory;
+};
+
+
+})();
